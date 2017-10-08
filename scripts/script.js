679875aa517cd7b89ad046d5a7b7679434cb8522
John Erickson <ericksonjohn08@hotmail.com>
2017-10-07 22:06:24 -0400

    Add two buttons to index. Add variables to access buttons and eventListeners to buttons in scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,70 @@
-var header, resetBtn, message, squares, colorDisplay, colors, pickedColor;
+var header, resetBtn, message, easyBtn, hardBtn, squares, colorDisplay, colors, numOfSquares, pickedColor;
 
 // dom selections
 header = document.querySelector('h1');
 resetBtn = document.getElementById('reset');
 message = document.getElementById('message');
+easyBtn = document.getElementById('easyBtn');
+hardBtn = document.getElementById('hardBtn');
 squares = document.querySelectorAll('.square');
 colorDisplay = document.getElementById('colorDisplay');
+numOfSquares = 6;
 
 resetBtn.addEventListener('click', function() {
 	header.style.backgroundColor = '#232323';
 	resetBtn.textContent = 'New Colors';
-	colors = generateRandomColors(6);
+	message.textContent = "";
+	hardBtn.classList.add('selected');
+	easyBtn.classList.remove('selected');
+		
+	numOfSquares = 6;
+	colors = generateRandomColors(numOfSquares);
 	pickedColor = randomColor();
 	colorDisplay.textContent = pickedColor;
 	
 	for (var i = 0; i < squares.length; i++) {
 		// add initial colors to squares
+		squares[i].style.display = 'block';
 		squares[i].style.backgroundColor = colors[i];
 	}
 });
 
-colors = generateRandomColors(6);
+easyBtn.addEventListener('click', function() {
+	hardBtn.classList.remove('selected');
+	easyBtn.classList.add('selected');
+	
+	numOfSquares = 3;
+	colors = generateRandomColors(numOfSquares);
+	pickedColor = randomColor();
+	colorDisplay.textContent = pickedColor;
+		
+	for (var i = 0; i < squares.length; i++) {
+		// add initial colors to squares
+		if (colors[i]) {
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = 'none';
+		}
+	}
+});
+
+hardBtn.addEventListener('click', function() {
+	easyBtn.classList.remove('selected');
+	hardBtn.classList.add('selected');
+	
+	numOfSquares = 6;
+	colors = generateRandomColors(numOfSquares);
+	pickedColor = randomColor();
+	colorDisplay.textContent = pickedColor;
+		
+	for (var i = 0; i < squares.length; i++) {
+		// add initial colors to squares
+		squares[i].style.display = 'block';
+		squares[i].style.backgroundColor = colors[i];
+	}
+});
+
+colors = generateRandomColors(numOfSquares);
 pickedColor = randomColor();
 colorDisplay.textContent = pickedColor;
 
